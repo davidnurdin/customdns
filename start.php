f5cf08c89497e2418b66b659ed44b223ab25e323
David Nurdin <david.nurdin@respawnsive.com>
2025-07-17 16:45:39 +0200

    update

--- start.php
+++ start.php
@@ -20,71 +20,6 @@ include 'src/Resolvers/ResolverInterface.php';
 include 'vendor/autoload.php';
 
 
-$client = new Clue\React\Docker\Client();
-
-$client->serviceList()->then(function (array $services) use ($client) {
-    foreach ($services as $service)
-    {
-        $client->taskList($service['ID'])->then(function (array $tasks) use ($service,$client) {
-            echo "Service: " . $service['Spec']['Name'] . PHP_EOL;
-//            echo "Tasks: " . count($tasks) . PHP_EOL;
-
-            foreach ($tasks as $task) {
-//                echo "=========> Task ID: " . $task['ID'] . PHP_EOL;
-
-                $client->taskInspect($task['ID'])->then(function (array $taskDetails) {
-
-                    if (isset($taskDetails['NetworksAttachments'][0]['Addresses'])) {
-                        if ($taskDetails['DesiredState'] == 'running') {
-                            var_dump($taskDetails['NetworksAttachments'][0]['Addresses']);
-//                            var_dump($taskDetails['DesiredState']);
-                        }
-
-                    }
-
-                })->otherwise(function (Exception $e) {
-                    echo 'Error inspecting task: ' . $e->getMessage() . PHP_EOL;
-                });
-
-
-                echo "Status: " . $task['Status']['State'] . PHP_EOL;
-                echo "Node: " . $task['NodeID'] . PHP_EOL;
-                echo PHP_EOL;
-            }
-        })->otherwise(function (Exception $e) {
-            echo 'Error listing tasks: ' . $e->getMessage() . PHP_EOL;
-        });
-
-
-        $client->serviceInspect($service['ID'])->then(function (array $serviceDetails) {
-        //    var_dump($serviceDetails);
-
-            echo PHP_EOL;
-        })->otherwise(function (Exception $e) {
-            echo 'Error inspecting service: ' . $e->getMessage() . PHP_EOL;
-        });
-
-    }
-}, function (Exception $e) {
-    echo 'Error: ' . $e->getMessage() . PHP_EOL;
-});
-
-
-//$client->containerInspect('symfony-docker-php-1')->then(function (array $container) {
-//    var_dump($container);
-//}, function (Exception $e) {
-//    echo 'Error: ' . $e->getMessage() . PHP_EOL;
-//});
-
-
-//$client->imageSearch('davidnurdin')->then(function (array $images) {
-//    var_dump($images);
-//}, function (Exception $e) {
-//    echo 'Error: ' . $e->getMessage() . PHP_EOL;
-//});
-
-
-
 class myResolver implements ResolverInterface
 {
 
@@ -92,28 +27,94 @@ class myResolver implements ResolverInterface
     {
         // Not use in overload
         // TODO: Implement getAnswer() method.
-        $debug = 1 ;
+        $debug = 1;
     }
 
-    public function getAnswerAsync(array $queries, ?string $client = null) : \React\Promise\PromiseInterface
+    public function getAnswerAsync(array $queries, ?string $client = null): \React\Promise\PromiseInterface
     {
-        $debug = 2 ;
+        $debug = 2;
 
         $deferred = new Deferred();
 
         // TODO: Implement getAnswer() method.
-        $debug = 1 ;
+        $debug = 1;
 
-        $answers = [] ;
-        $resolveInternal = false ;
+        $answers = [];
+        $resolveInternal = true;
         foreach ($queries as $query) {
             /** @var ResourceRecord $query */
             var_dump($query->getName());
             // if (true) {
+
+            if (str_starts_with($query->getName(), 'tasksActive.') && str_ends_with($query->getName(), '.')) {
+
+                $serviceName = substr($query->getName(),strlen('tasksActive.'), -1); // Remove 'tasksActive.' prefix and trailing dot
+
+                var_dump("=======+>". $serviceName);
+
+                $client = new Clue\React\Docker\Client();
+                $client->serviceList()->then(function (array $services) use ($client,$serviceName,$deferred,&$resolveInternal) {
+                    foreach ($services as $service) {
+                        $client->taskList($service['ID'])->then(function (array $tasks) use ($service, $client,$serviceName,$deferred,&$resolveInternal) {
+                            if ($service['Spec']['Name']  == $serviceName) {
+                                echo "Service: " . $service['Spec']['Name'] . PHP_EOL;
+//            echo "Tasks: " . count($tasks) . PHP_EOL;
+
+                                foreach ($tasks as $task) {
+//                echo "=========> Task ID: " . $task['ID'] . PHP_EOL;
+
+                                    $client->taskInspect($task['ID'])->then(function (array $taskDetails) use ($deferred,&$resolveInternal,$service) {
+
+                                        if (isset($taskDetails['NetworksAttachments'][0]['Addresses'])) {
+                                            if ($taskDetails['Status']['State'] == 'running') {
+
+                                                $ipRange = $taskDetails['NetworksAttachments'][0]['Addresses'] ;
+                                                $ip = explode('/', $ipRange[0])[0]; // Get the IP address part before the slash
+                                                $answers[] = (new ResourceRecord())
+                                                    ->setQuestion(false)
+                                                    ->setTtl(1)
+                                                    ->setType(RecordTypeEnum::TYPE_A)
+                                                    ->setName('tasksActive.' . $service['Spec']['Name']. '.')
+                                                    ->setRdata($ip);
+
+                                                $deferred->resolve($answers);
+                                                $resolveInternal = true ;
+
+
+                                                var_dump($taskDetails['NetworksAttachments'][0]['Addresses']);
+
+//                            var_dump($taskDetails['DesiredState']);
+                                            }
+
+                                        }
+
+                                    })->otherwise(function (Exception $e) {
+                                        echo 'Error inspecting task: ' . $e->getMessage() . PHP_EOL;
+                                    });
+
+
+                                    echo "Status: " . $task['Status']['State'] . PHP_EOL;
+                                    echo "Node: " . $task['NodeID'] . PHP_EOL;
+                                    echo PHP_EOL;
+                                }
+                            }
+                        })->otherwise(function (Exception $e) {
+                            echo 'Error listing tasks: ' . $e->getMessage() . PHP_EOL;
+                        });
+
+
+
+                    }
+                }, function (Exception $e) {
+                    echo 'Error: ' . $e->getMessage() . PHP_EOL;
+                });
+
+
+            }
             if ($query->getName() === "david.") {
                 // If the query is for "david", return the answer
 
-                $resolveInternal = true ;
+                $resolveInternal = true;
                 $config = React\Dns\Config\Config::loadSystemConfigBlocking();
                 $config->nameservers = ['8.8.8.8'];
 //                if (!$config->nameservers) {
@@ -123,14 +124,13 @@ class myResolver implements ResolverInterface
                 $dns = $factory->create($config);
 
                 //if ($query->getType() ==
-                $host = "google.fr" ;
-                $host = substr($query->getName(),0,-1);
-                var_dump("TYPE:". $query->getType());
+                $host = "google.fr";
+                $host = substr($query->getName(), 0, -1);
+                var_dump("TYPE:" . $query->getType());
 
                 // $dns->resolveAll ??
 
-                if ($host == 'david')
-                {
+                if ($host == 'david') {
                     $answers[] = (new ResourceRecord())
                         ->setQuestion(false)
                         ->setTtl(1)
@@ -139,8 +139,7 @@ class myResolver implements ResolverInterface
                         ->setRdata('6.6.6.6');
 
                     $deferred->resolve($answers);
-                }
-                else {
+                } else {
                     if ($query->getType() !== 1)
                         $deferred->reject(new RefusedTypeException("Unsupported type"));
                     else {
@@ -178,17 +177,17 @@ class myResolver implements ResolverInterface
     public function allowsRecursion(): bool
     {
         // TODO: Implement allowsRecursion() method.
-        $debug = 1 ;
-        return false ;
+        $debug = 1;
+        return false;
     }
 
     public function isAuthority(string $domain): bool
     {
         // TODO: Implement isAuthority() method.
         if ($domain == "david.")
-            return true ;
+            return true;
 
-        return false ;
+        return false;
     }
 }
 
@@ -199,12 +198,12 @@ class RefusedTypeException extends \Exception
 
 class FactoryExtended extends Factory
 {
-    public function createServer($address,$context = null)
+    public function createServer($address, $context = null)
     {
         $loop = $this->loop;
 
-        return $this->resolveAddress($address)->then(function ($address) use ($loop,$context) {
-            $socket = @\stream_socket_server($address, $errno, $errstr, \STREAM_SERVER_BIND,$context);
+        return $this->resolveAddress($address)->then(function ($address) use ($loop, $context) {
+            $socket = @\stream_socket_server($address, $errno, $errstr, \STREAM_SERVER_BIND, $context);
 
             if (!$socket) {
                 throw new Exception('Unable to create server socket: ' . $errstr, $errno);
@@ -215,6 +214,7 @@ class FactoryExtended extends Factory
     }
 
 }
+
 class ServerExtended extends \CatFerq\ReactPHPDNS\Server
 {
 
@@ -226,7 +226,7 @@ class ServerExtended extends \CatFerq\ReactPHPDNS\Server
             )
         ));
 
-        parent::__construct($resolver, $ip, $port, $loop,$context);
+        parent::__construct($resolver, $ip, $port, $loop, $context);
     }
 
     public function onMessage(string $message, string $address, SocketInterface $socket): void
