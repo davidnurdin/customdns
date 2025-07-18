bc5152157d8c8fb99ed3cbf1b62f5c39a734f27a
david nurdin <david.nurdin@respawnsive.com>
2025-07-18 08:39:24 +0000

    todolist

--- clear_images.sh
+++ clear_images.sh
@@ -1,3 +1,4 @@
+docker stack rm dns
 ssh debian@swarm1.respawnsive.net sudo docker image rm davidnurdin/customdns
 ssh debian@swarm2.respawnsive.net sudo docker image rm davidnurdin/customdns
 ssh debian@swarm3.respawnsive.net sudo docker image rm davidnurdin/customdns
