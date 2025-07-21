172eb0736177d0203fb7eff5b18f7e655f21ea2e
david nurdin <david.nurdin@respawnsive.com>
2025-07-21 08:18:55 +0000

    update

--- clear_images.sh
+++ clear_images.sh
@@ -1,2 +1,4 @@
 ./delete.sh
-ssh debian@swarm1.respawnsive.net sudo docker image prune
+ssh debian@swarm1.respawnsive.net sudo docker image prune -f
+ssh debian@swarm2.respawnsive.net sudo docker image prune -f
+ssh debian@swarm3.respawnsive.net sudo docker image prune -f
