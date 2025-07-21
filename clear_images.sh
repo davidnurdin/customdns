cafd81f8435655b236c49db405e6edcbee49fcc1
david nurdin <david.nurdin@respawnsive.com>
2025-07-21 08:02:23 +0000

    update

--- clear_images.sh
+++ clear_images.sh
@@ -1,4 +1,2 @@
-docker stack rm dns
-ssh debian@swarm1.respawnsive.net sudo docker image rm davidnurdin/customdns
-ssh debian@swarm2.respawnsive.net sudo docker image rm davidnurdin/customdns
-ssh debian@swarm3.respawnsive.net sudo docker image rm davidnurdin/customdns
+./delete.sh
+ssh debian@swarm1.respawnsive.net sudo docker image prune
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-docker stack rm dns
 docker stack deploy dns --compose-file docker-compose.yml
