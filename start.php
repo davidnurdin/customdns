5c902e2d4e956acd95df94093a9eca5bb079d03b
David Nurdin <david.nurdin@respawnsive.com>
2025-07-25 12:48:03 +0200

    update

--- start.php
+++ start.php
@@ -490,6 +490,7 @@ class ServerExtended extends \CatFerq\ReactPHPDNS\Server
                             // search the client source in this list with same IPV4
                             if (explode('/',$containerInfos['IPv4Address'])[0] == explode(':',$data['infos']['client'])[0]) {
                                 echo "Found client in docker_gwbridge network: " . $containerName . PHP_EOL; // container web
+                                // TODO : faire la correspondance $client <> ip du container sur le meme réseau !
 
                             }
 
