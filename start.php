247e98cd30adb4d6fa15a0b15d9000b070c574b5
David Nurdin <david.nurdin@respawnsive.com>
2025-07-25 10:35:12 +0200

    update

--- start.php
+++ start.php
@@ -298,7 +298,7 @@ class ServerExtended extends \CatFerq\ReactPHPDNS\Server
 
     }
 
-    public function testIpConnectivity($domain, ?string $idTimer = null)
+    public function testIpConnectivity($domain, string $idTimer = null)
     {
         global $_CACHE;
 
