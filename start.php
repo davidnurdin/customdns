b2c88080263be93bc64b4b13604b3cea071fe732
David Nurdin <david.nurdin@respawnsive.com>
2025-07-25 11:18:39 +0200

    update

--- start.php
+++ start.php
@@ -499,7 +499,7 @@ class ServerExtended extends \CatFerq\ReactPHPDNS\Server
 
 
 
-                                    var_dump($data['client']);die();
+                                    var_dump($data['infos']['client']);die();
 
                                     $_CACHE[$data['infos']['domain']]['nbTasksToResolve'] = count($tasks);
                                     $_CACHE[$data['infos']['domain']]['nbTasksResolved'] = 0;
