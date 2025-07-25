a499041a83b1d83331b1b838f0abec141034d16b
David Nurdin <david.nurdin@respawnsive.com>
2025-07-25 12:36:01 +0200

    update

--- start.php
+++ start.php
@@ -497,7 +497,7 @@ class ServerExtended extends \CatFerq\ReactPHPDNS\Server
                                 // TODO : ne connect que si le current node est bien le meme !
                                 echo "Network: " . $network['NetworkID'] . PHP_EOL;
                                 echo "Addr:" . $network['Addr'] . PHP_EOL;
-                                echo "Try to connect to network: " . $network['NetworkID'] . " On container : " . $task['Status']['ContainerStatus']['ContainerID'] . PHP_EOL;
+                                echo "Try to connect to network: " . $network['NetworkID'] . " On container : " . $containerID . PHP_EOL;
                                 // ASK DNS HELPER to join NETWORK
                                 $client->networkConnect($network['NetworkID'], $task['Status']['ContainerStatus']['ContainerID'])->then(function () use ($service, $client, $serviceName, $data, &$_CACHE, &$_TORESEND) {
                                     echo "Connected to network: " . $service['Spec']['Name'] . PHP_EOL;
