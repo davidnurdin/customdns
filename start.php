2e4341e6cc39ae8b57d6e3dfcea131530350357a
David Nurdin <david.nurdin@respawnsive.com>
2025-07-30 11:44:04 +0200

    update

--- start.php
+++ start.php
@@ -689,8 +689,6 @@ class ServerExtended extends \CatFerq\ReactPHPDNS\Server
                                 $client->taskList($service['ID'])->then(function (array $tasks) use (&$_TORESOLVE,$service, $client, $serviceName, $data, &$_CACHE, &$_TORESEND,$resolverClientContainerId,$ipAsker,$domain) {
                                     echo "Service: " . $service['Spec']['Name'] . PHP_EOL;
 
-                                    var_dump($tasks);
-                                    die();
                                     
                                     // filter task get only Running AND have Addresses
                                     $tasks = array_filter($tasks, function ($task) {
